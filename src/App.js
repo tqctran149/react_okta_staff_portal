a2d5e87c0042977011fd0a9f6ea58ac3e8cafe96
tqctran149 <tqctran149@gmail.com>
2020-02-10 18:21:03 -0500

    added home page and staff page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+
+import Navbar from './components/layout/Navbar';
+import Home from './components/pages/Home';
+import Staff from './components/pages/Staff';
+
 
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className = "App">
+          <Navbar />
+          <div className = "container">
+          <Route path = "/" exact = {true} component = {Home} />
+          <Route path = "/staff" exact = {true} component = {Staff} />
+          </div>
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
